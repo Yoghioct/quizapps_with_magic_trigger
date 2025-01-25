a8c84920fff2cc30cc4db222ef5031a8fd53980d
Yoghi Oktapiansyah <92005366+Yoghioct@users.noreply.github.com>
2025-01-25 09:27:34 +0700

    Update leaderboard.blade.php

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -187,7 +187,7 @@
                             });
                             this.loading = false;
                         });
-                }, 3000);
+                }, 30000);
             },
             getMovement(id, newIndex) {
                 const previousIndex = this.previousState[id];
